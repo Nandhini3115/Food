d0e989c410e079f591bcbd09b3a4c459350f4a0b
Nandhini3115 <jnandhini3115@gmail.com>
2026-01-04 01:04:01 +0530

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,6 +1,10 @@
+const cors = require("cors");
 const express=require('express');
 const app=express();
 const PORT = 3000;
+
+app.use(cors());
+
 app.use(express.json());
 const foodRoutes=require('./routes/foodRoutes');
 app.use('/foods',foodRoutes);
